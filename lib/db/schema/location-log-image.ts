ad8e85a44fb9634505457d45e162c891f0d56083
Rafael <devrafaelffz@gmail.com>
2025-05-05 23:09:32 -0300

    :sparkles: setup auth

--- lib/db/schema/location-log-image.ts
+++ lib/db/schema/location-log-image.ts
@@ -1,5 +1,6 @@
 import { int, sqliteTable, text } from "drizzle-orm/sqlite-core";
 
+import { user } from "./auth";
 import { locationLog } from "./location-log";
 
 export const locationLogImage = sqliteTable("location_log_image", {
@@ -8,6 +9,9 @@ export const locationLogImage = sqliteTable("location_log_image", {
   locationLogId: int()
     .notNull()
     .references(() => locationLog.id),
+  userId: int()
+    .notNull()
+    .references(() => user.id),
   createdAt: int()
     .notNull()
     .$default(() => Date.now()),
