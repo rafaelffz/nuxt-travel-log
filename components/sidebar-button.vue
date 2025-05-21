51f0218e1c824620ab465e48526a0612b090f602
Rafael <108911706+rafaelffz@users.noreply.github.com>
2025-05-21 01:33:49 -0300

    :sparkles: add dashboard sidebar (#12)

---
+++ components/sidebar-button.vue
@@ -0,0 +1,57 @@
+<script setup lang="ts">
+const props = defineProps<{
+  label: string;
+  icon: string;
+  href: string;
+  showLabel?: boolean;
+}>();
+
+const route = useRoute();
+</script>
+
+<template>
+  <div
+    class="tooltip tooltip-right"
+    :data-tip="showLabel ? undefined : props.label"
+  >
+    <NuxtLink
+      :to="props.href"
+      :class="{
+        'bg-base-300': route.path === props.href,
+      }"
+      class="flex items-center gap-2 p-2 hover:bg-base-300 rounded-md cursor-pointer font-medium"
+    >
+      <Icon :name="props.icon" size="24" class="mx-1 min-h-6 min-w-6" />
+      <Transition name="grow-x">
+        <span
+          v-show="showLabel"
+          class="whitespace-nowrap origin-left" :class="[
+            showLabel ? 'w-auto opacity-100' : 'w-0 opacity-0',
+          ]"
+        >{{ props.label }}</span>
+      </Transition>
+    </NuxtLink>
+  </div>
+</template>
+
+<style scoped>
+.grow-x-enter-active,
+.grow-x-leave-active {
+  transition:
+    transform 0.2s ease,
+    opacity 0.2s ease;
+  transform-origin: left;
+}
+
+.grow-x-enter-from,
+.grow-x-leave-to {
+  transform: scaleX(0);
+  opacity: 0;
+}
+
+.grow-x-enter-to,
+.grow-x-leave-from {
+  transform: scaleX(1);
+  opacity: 1;
+}
+</style>
--- pages/sign-out.vue
+++ pages/sign-out.vue
@@ -1,8 +1,8 @@
 <script setup lang="ts">
 const authStore = useAuthStore();
 
-onMounted(() => {
-  authStore.signOut();
+onMounted(async () => {
+  await authStore.signOut();
 });
 </script>
 
