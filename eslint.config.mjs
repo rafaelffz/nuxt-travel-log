3574c369d95b9c717371ea542ffea47528d3a36b
Rafael <devrafaelffz@gmail.com>
2025-05-24 01:18:23 -0300

    :sparkles: add add location page

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -19,6 +19,17 @@ export default withNuxt(
     },
     {
       rules: {
+        "vue/max-attributes-per-line": [
+          "error",
+          {
+            singleline: {
+              max: 3,
+            },
+            multiline: {
+              max: 1,
+            },
+          },
+        ],
         "ts/no-redeclare": "off",
         "ts/consistent-type-definitions": ["error", "type"],
         "no-console": ["warn"],
--- server/middleware/auth.ts
+++ server/middleware/auth.ts
@@ -1,12 +1,16 @@
+import type { UserWithId } from "~/lib/auth";
+
 import { auth } from "~/lib/auth";
 
 export default defineEventHandler(async (event) => {
-  if (event.path.startsWith("/dashboard")) {
-    const session = await auth.api.getSession({
-      headers: event.headers,
-    });
+  const session = await auth.api.getSession({
+    headers: event.headers,
+  });
 
-    if (!session) {
+  event.context.user = session?.user as unknown as UserWithId;
+
+  if (event.path.startsWith("/dashboard")) {
+    if (!session?.user) {
       await sendRedirect(event, "/", 302);
     }
   }
